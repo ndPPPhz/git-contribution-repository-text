97577bc08355fb7eb841126261ff8758eefb3180
ndPPPhz <andp92@gmail.com>
2020-03-26 18:09:37 +0000

    Remove default color as default parameter, handle empty spaces

--- draw.js
+++ draw.js
@@ -1,8 +1,8 @@
-default_color = '#239a3b';
-background_color = '#ebedf0';
-word = "ANNINO";
+function apply(w, bg_color, color) {
+	letter_color = color == null ? '#239a3b' : color;
+	background_color = bg_color == null ? '#ebedf0' : bg_color;
+	word = w == null ? ' HELLO' : w.toUpperCase();
 
-function apply() {
 	// querySelecor returns an object (HTMLCollection)
 	const contributions = document.querySelector('g[data-hydro-click-hmac]');
 	// Convert it to an array containing all the children nodes
@@ -49,19 +49,23 @@ function drawBlock(block, letter) {
 	};
 
 	insetBlock(block);
-	this[`draw${letter}`](block, default_color);
+	if (letter == " ") {
+		fill(block, background_color);
+	} else {
+		this[`draw${letter}`](block, letter_color);
+	}
 }
 
 function range(length, from) {
 	return Array.from({length: length}, (_, n) => n+from);
 }
 
-function drawA(block, color = default_color) {
+function drawA(block, color) {
 	drawF(block, color);
 	drawFullLastVertical(block, color); // |
 }
 
-function drawB(block, color = default_color) {
+function drawB(block, color) {
 	drawFullFirstVertical(block, color); // |
 	range(2,1).forEach((n) => {
 		set(block, n, 0, color);
@@ -72,12 +76,12 @@ function drawB(block, color = default_color) {
 	set(block, 3, 3, color);
 }
 
-function drawC(block, color = default_color) {
+function drawC(block, color) {
 	drawL(block, color);
 	drawFullFirstHorizontal(block, color);
 }
 
-function drawD(block, color = default_color) {
+function drawD(block, color) {
 	drawFullFirstVertical(block, color); // |
 	range(3,1).forEach((n) => {
 		set(block, n, 0, color); // -
@@ -86,18 +90,18 @@ function drawD(block, color = default_color) {
 	});
 }
 
-function drawE(block, color = default_color) {
+function drawE(block, color) {
 	drawF(block, color);
 	drawFullLastHorizontal(block, color);
 }
 
-function drawF(block, color = default_color) {
+function drawF(block, color) {
 	drawFullFirstHorizontal(block, color);
 	drawFullFirstVertical(block, color);
 	drawFullMidHorizontal(block, color);
 }
 
-function drawG(block, color = default_color) {
+function drawG(block, color) {
 	drawC(block, color);
 	range(2,0).forEach((n) => {
 		set(block, block.length - 1, 2+n, color); // |
@@ -105,25 +109,25 @@ function drawG(block, color = default_color) {
 	});
 }
 
-function drawH(block, color = default_color) {
+function drawH(block, color) {
 	drawFullMidHorizontal(block, color);
 	drawFullFirstVertical(block, color);
 	drawFullLastVertical(block, color);
 }
 
-function drawI(block, color = default_color) {
+function drawI(block, color) {
 	drawT(block, color);
 	drawFullLastHorizontal(block, color);
 }
 
-function drawJ(block, color = default_color) {
+function drawJ(block, color) {
 	drawT(block, color);
 	range(2,0).forEach((n) => {
 		set(block, n, block.length - 1, color); // _
 	});
 }
 
-function drawK(block, color = default_color) {
+function drawK(block, color) {
 	range(block.length,0).forEach((n) => {
 		set(block, 0, n, color); // |
 	});
@@ -134,12 +138,12 @@ function drawK(block, color = default_color) {
 	});
 }
 
-function drawL(block, color = default_color) {
+function drawL(block, color) {
 	drawFullFirstVertical(block, color);
 	drawFullLastHorizontal(block, color);
 }
 
-function drawM(block, color = default_color) {
+function drawM(block, color) {
 	drawFullFirstVertical(block, color);
 	drawFullLastVertical(block, color);
 	set(block, 1,0,color);
@@ -147,35 +151,35 @@ function drawM(block, color = default_color) {
 	set(block, 2,1,color);
 }
 
-function drawN(block, color = default_color) {
+function drawN(block, color) {
 	drawFullFirstVertical(block, color);
 	drawFullLastVertical(block, color);
 	drawFull13Bisect(block, color);
 }
 
-function drawO(block, color = default_color) {
+function drawO(block, color) {
 	drawC(block,color);
 	drawFullLastVertical(block, color);
 }
 
-function drawP(block, color = default_color) {
+function drawP(block, color) {
 	drawF(block, color);
 	range(2,0).forEach((n) => {
 		set(block, block.length - 1, n, color); // _
 	});
 }
 
-function drawQ(block, color = default_color) {
+function drawQ(block, color) {
 	drawO(block, color);
 	drawFinalHalf13Bisect(block, color);
 }
 
-function drawR(block, color = default_color) {
+function drawR(block, color) {
 	drawP(block, color);
 	drawFinalHalf13Bisect(block, color);
 }
 
-function drawS(block, color = default_color) {
+function drawS(block, color) {
 	drawFullFirstHorizontal(block,color);
 	drawFullLastHorizontal(block,color);
 	drawFullMidHorizontal(block, color);
@@ -183,17 +187,17 @@ function drawS(block, color = default_color) {
 	set(block,4,3, color);
 }
 
-function drawT(block, color = default_color) {
+function drawT(block, color) {
 	drawFullFirstHorizontal(block,color);
 	drawFullMidVertical(block, color);
 }
 
-function drawU(block, color = default_color) {
+function drawU(block, color) {
 	drawL(block, color);
 	drawFullLastVertical(block, color);
 }
 
-function drawV(block, color = default_color) {
+function drawV(block, color) {
 	range(3,0).forEach((n) => {
 		set(block, 0, n, color); // |
 		set(block, block.length - 1, n, color); // |
@@ -205,7 +209,7 @@ function drawV(block, color = default_color) {
 	});
 }
 
-function drawW(block, color = default_color) {
+function drawW(block, color) {
 	range(4,0).forEach((n) => {
 		set(block, 0, n, color); // |
 		set(block, block.length - 1, n, color); // |
@@ -215,79 +219,79 @@ function drawW(block, color = default_color) {
 	set(block, 2,3, color);
 }
 
-function drawX(block, color = default_color) {
+function drawX(block, color) {
 	drawY(block, color);
 	drawFinalHalf13Bisect(block,color);
 }
 
-function drawY(block, color = default_color) {
+function drawY(block, color) {
 	drawFull24Bisect(block, color);
 	range(3,0).forEach((n) => {
 		set(block, n, n, color); // \
 	});
 }
 
-function drawZ(block, color = default_color) {
+function drawZ(block, color) {
 	drawFullFirstHorizontal(block,color);
 	drawFullLastHorizontal(block,color);
 	drawFull24Bisect(block, color);
 }
 
-function drawFullFirstVertical(block, color = default_color) {
+function drawFullFirstVertical(block, color) {
 	range(block.length,0).forEach((n) => {
 		set(block, 0, n, color); // |
 	});
 }
 
-function drawFullLastVertical(block, color = default_color) {
+function drawFullLastVertical(block, color) {
 	range(block.length,0).forEach((n) => {
 		set(block, block.length -1, n, color);
 	});
 }
 
-function drawFullFirstHorizontal(block, color = default_color) {
+function drawFullFirstHorizontal(block, color) {
 	range(block.length,0).forEach((n) => {
 		set(block, n, 0, color);
 	});
 }
 
-function drawFullLastHorizontal(block, color = default_color) {
+function drawFullLastHorizontal(block, color) {
 	range(block.length,0).forEach((n) => {
 		set(block, n, block.length -1, color);
 	});
 }
 
-function drawFullMidHorizontal(block, color = default_color) {
+function drawFullMidHorizontal(block, color) {
 	range(block.length,0).forEach((n) => {
 		set(block, n, Math.floor((block.length -1) / 2), color);
 	});
 }
 
-function drawFullMidVertical(block, color = default_color) {
+function drawFullMidVertical(block, color) {
 	range(block.length,0).forEach((n) => {
 		set(block, Math.floor((block.length -1) / 2), n, color);
 	});
 }
 
-function drawFull13Bisect(block, color = default_color) {
+function drawFull13Bisect(block, color) {
 	range(block.length,0).forEach((n) => {
 		set(block, n, n, color);
 	});
 }
 
-function drawFull24Bisect(block, color = default_color) {
+function drawFull24Bisect(block, color) {
 	range(block.length,0).forEach((n) => {
 		set(block, n,  block.length - 1 - n, color);
 	});
 }
 
-function drawFinalHalf13Bisect(block, color = default_color) {
+function drawFinalHalf13Bisect(block, color) {
 	range(3,0).forEach((n) => {
 		set(block, block.length - 1 - n, block.length - 1 - n, color); // \
 	});
 }
 
-function fill(block, color = default_color) {
+function fill(block, color) {
 	 block.forEach((x_week) => {
 	   x_week.days.forEach((y_day) => {
 		   y_day.setAttribute('fill', color);
@@ -295,6 +299,6 @@ function fill(block, color = default_color) {
    });
 }
 
-function set(block, x, y, color = default_color) {
+function set(block, x, y, color) {
 	block[x].days[y].setAttribute('fill', color);
 }
